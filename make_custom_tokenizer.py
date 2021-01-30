ac1c6470f8c9c8047897dd5827388248823f7b48
yeongjae-yu <yyj3053@gmail.com>
2021-01-31 03:28:03 +0900

    [20210131] refactoring code and make new vocab

--- make_custom_tokenizer.py
+++ make_custom_tokenizer.py
@@ -5,34 +5,39 @@ import csv
 
 SRC_DATA_PATH = '../integrated_data/korean_for_nlp.txt'
 
-# request.urlretrieve("https://raw.githubusercontent.com/LawrenceDuan/IMDb-Review-Analysis/master/IMDb_Reviews.csv",
-#                     filename="IMDb_Reviews.csv")
-
-# train_df = pd.read_csv('IMDb_Reviews.csv')
-# print(len(train_df))
-# with open('imdb_review.txt', 'w', encoding='utf8') as f:
-#     f.write('\n'.join(train_df['review']))
-
 
 if __name__ == '__main__':
-    spm.SentencePieceTrainer.Train('--input=%s --model_prefix=spm_unigram --vocab_size=32000'
-                                   '--model_type=unigram --max_sentence_length=9999' % SRC_DATA_PATH)
+    input = SRC_DATA_PATH
+    vocab_size = '8000'
+    model_type = 'unigram'
+    model_prefix = 'spm_%s_%s' % (model_type, vocab_size)
+    max_sentence_length = '9999'
 
-    vocab_list = pd.read_csv('spm_unigram.vocab', sep='\t', header=None, quoting=csv.QUOTE_NONE)
+
+    # spm.SentencePieceTrainer.Train('--input=%s --model_prefix=%s --vocab_size=%s'
+    #                                ' --model_type=%s --max_sentence_length=%s'
+    #                                ' --pad_id=0 --pad_piece=[PAD]'
+    #                                ' --unk_id=1 --unk_piece=[UNK]'
+    #                                ' --bos_id=2 --bos_piece=[BOS]'
+    #                                ' --eos_id=3 --eos_piece=[EOS]' % (
+    #                                 input, model_prefix, vocab_size, model_type, max_sentence_length))
+
+    vocab_list = pd.read_csv('%s.vocab' % model_prefix, sep='\t', header=None, quoting=csv.QUOTE_NONE)
     print(vocab_list[:10])
 
     sp = spm.SentencePieceProcessor()
-    vocab_file = "imdb.model"
+    vocab_file = "%s.model" % model_prefix
     sp.load(vocab_file)
 
     lines = [
-      "I didn't at all think of it this way.",
-      "t t 쀏"
+      '나는 안녕하세요 1+1 이벤트 진행 중이다, 가격 1300원이야.',
+      "t 값이 15,021원입니다."
     ]
     for line in lines:
-        line = sp.IdToPiece(1) + line + sp.IdToPiece(2)
+        line = sp.IdToPiece(2) + line + sp.IdToPiece(3)
         print(line)
         print(sp.encode_as_pieces(line))
         print(sp.encode_as_ids(line))
         print()
-    print(sp.IdToPiece(1))
+    print(sp.IdToPiece(5))
+    print(sp.piece_to_id('[BOS]'))
